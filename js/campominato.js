31c5450465af3404a5a8b81ae5acc8c0692002ea
dieghino26 <brotzadiego@gmail.com>
2024-01-11 19:40:46 +0100

    added score info

--- js/campominato.js
+++ js/campominato.js
@@ -1,8 +1,7 @@
-//recuperare la griglia da html
+// recupero elementi da html
 const grid = document.getElementById("grid")
-
-//recupero il bottone play
 const button = document.getElementById("button")
+const playerScore = document.getElementById("score")
 
 //rows e cols
 const rows = 10
@@ -10,11 +9,20 @@ const cols = 10
 const totalCells = rows * cols
 
 
+
+
+
 button.addEventListener("click", function () {
 
     button.innerText = "Ricomincia"
     grid.innerHTML = ""
 
+    //info bombe
+    let totalBombs = 16
+
+    //score del player
+    let score = 0
+    playerScore.innerText = score
 
     //cell generator
     for (let i = 0; i < totalCells; i++) {
